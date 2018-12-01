58b8e5c7488d7a2692d65c3f92988ca60cbf87ee
trungnx <trung@kyber.network>
2018-12-01 20:25:56 +0700

    Integrate KyberWidget

--- mvp/client-mvp/app/reducers/courseReducer.js
+++ mvp/client-mvp/app/reducers/courseReducer.js
@@ -2,11 +2,21 @@ export const initialState = {
   courses: [
     {
       id: 1,
-      name: "Course 1"
+      name: "AWS Certified Solutions Architect - Associate",
+      image: "https://udemy-images.udemy.com/course/480x270/362328_91f3_10.jpg",
+      price: 600
     },
     {
       id: 2,
-      name: "Course 1"
+      name: "Learn 3D Modelling for Beginners",
+      image: "https://udemy-images.udemy.com/course/240x135/438522_500f_6.jpg",
+      price: 1500
+    },
+    {
+      id: 3,
+      name: "The Complete Ethical Hacking Course: Beginner to Advanced",
+      image: "https://udemy-images.udemy.com/course/480x270/437490_c76a_4.jpg",
+      price: 2700
     }
   ]
 };
