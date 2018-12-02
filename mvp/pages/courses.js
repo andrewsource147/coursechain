6890c760cdb512bd0820ea6652949cdc2619591e
trungnx <trung@kyber.network>
2018-12-02 09:32:21 +0700

    add marked

--- mvp/pages/courses.js
+++ mvp/pages/courses.js
@@ -26,9 +26,11 @@ class Courses extends Component {
             <div className={"container"}>
               <h3 className={"courses__body-title"}>Featured Courses</h3>
               <div className={"courses__course-container"}>
+                <Link href={{ pathname: '/course', query: { id: 1 } }}>
                 <div className={"courses__course"}>
                   <div className={"courses__course-background"} style={{backgroundImage: "url(/static/background-course-1.png)"}}/>
                   <div className={"courses__course-data"}>
+                    <div className={"courses__course-marked"}>Best Seller</div>
                     <div className={"courses__course-lecturer-avatar"} style={{backgroundImage: "url(/static/expert-1.svg)"}}/>
                     <div className={"courses__course-title"}>AWS Certified Solutions Architect - Associate</div>
                     <div className={"courses__course-price"}>$10</div>
@@ -40,10 +42,13 @@ class Courses extends Component {
                     </Link>
                   </div>
                 </div>
+                </Link>
 
+                <Link href={{ pathname: '/course', query: { id: 2 } }}>
                 <div className={"courses__course"}>
                   <div className={"courses__course-background"} style={{backgroundImage: "url(/static/background-course-2.png)"}}/>
                   <div className={"courses__course-data"}>
+                    <div className={"courses__course-marked courses__course-marked--yellow"}>Highest Rated</div>
                     <div className={"courses__course-lecturer-avatar"} style={{backgroundImage: "url(/static/expert-2.svg)"}}/>
                     <div className={"courses__course-title"}>AWS Certified Solutions Architect - Associate</div>
                     <div className={"courses__course-price"}>$10</div>
@@ -55,7 +60,9 @@ class Courses extends Component {
                     </Link>
                   </div>
                 </div>
+                </Link>
 
+                <Link href={{ pathname: '/course', query: { id: 3 } }}>
                 <div className={"courses__course"}>
                   <div className={"courses__course-background"} style={{backgroundImage: "url(/static/background-course-3.png)"}}/>
                   <div className={"courses__course-data"}>
@@ -70,6 +77,7 @@ class Courses extends Component {
                     </Link>
                   </div>
                 </div>
+                </Link>
               </div>
             </div>
           </div>
